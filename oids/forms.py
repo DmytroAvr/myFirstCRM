5b4fcc81337ab52e63f07df608dc94000b9194ee
Dmytro <dnytro9avr@gmail.com>
2025-05-17 21:09:37 +0300

    try ad multiDocAdd

--- oids/forms.py
+++ oids/forms.py
@@ -1,25 +1,33 @@
 # C:\myFirstCRM\oids\forms.py
 from django import forms
-from .models import Document, OID
+from .models import Document, OID, Unit
 from django.forms import modelformset_factory
 
 
+# C:\myFirstCRM\oids\forms.py
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
-        fields = '__all__'
+        fields = ['document_type', 'document_number', 'process_date', 'work_date', 'author', 'note']
+
+class DocumentHeaderForm(forms.Form):
+    unit = forms.ModelChoiceField(queryset=Unit.objects.all(), label="Військова частина")
+    oid = forms.ModelChoiceField(queryset=OID.objects.none(), label="Об'єкт")
+    work_type = forms.ChoiceField(choices=Document.WORK_TYPE_CHOICES, label="Тип роботи")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['oid'].queryset = OID.objects.none()
-
         if 'unit' in self.data:
             try:
                 unit_id = int(self.data.get('unit'))
                 self.fields['oid'].queryset = OID.objects.filter(unit_id=unit_id).exclude(status='скасовано').order_by('name')
             except (ValueError, TypeError):
                 pass
-        elif self.instance.pk:
-            self.fields['oid'].queryset = self.instance.unit.oid_set.exclude(status='скасовано').order_by('name')
 
 
+DocumentFormSet = modelformset_factory(
+    Document,
+    form=DocumentForm,
+    extra=5,
+    can_delete=True
+)
--- oids/views.py
+++ oids/views.py
@@ -5,7 +5,7 @@ from .forms import DocumentForm
 from django.http import JsonResponse
 import traceback        #check
 
-def document_create(request):
+def document_create33(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST)
         if form.is_valid():
@@ -15,14 +15,7 @@ def document_create(request):
         form = DocumentForm()
     return render(request, 'oids/document_form.html', {'form': form})
 
-# def load_oids(request):
-#     unit_id = request.GET.get('unit')
-#     oids = OID.objects.filter(unit_id=unit_id).exclude(status='скасовано').order_by('name')
-#     return JsonResponse(list(oids.values('id', 'name')), safe=False)
 
-
-
-# for checking
 def load_oids(request):
     try:
         unit_id = request.GET.get('unit')
@@ -31,3 +24,36 @@ def load_oids(request):
     except Exception as e:
         traceback.print_exc()  # ← виведе помилку у консоль VSCode
         return JsonResponse({'error': str(e)}, status=500)
+
+
+
+
+# C:\myFirstCRM\oids\views.py
+from .forms import DocumentHeaderForm, DocumentFormSet
+
+def document_create(request):
+    if request.method == 'POST':
+        header_form = DocumentHeaderForm(request.POST)
+        formset = DocumentFormSet(request.POST)
+
+        if header_form.is_valid() and formset.is_valid():
+            unit = header_form.cleaned_data['unit']
+            oid = header_form.cleaned_data['oid']
+            work_type = header_form.cleaned_data['work_type']
+
+            for form in formset:
+                if form.cleaned_data and not form.cleaned_data.get('DELETE', False):
+                    doc = form.save(commit=False)
+                    doc.unit = unit
+                    doc.oid = oid
+                    doc.work_type = work_type
+                    doc.save()
+            return redirect('document_create')
+    else:
+        header_form = DocumentHeaderForm()
+        formset = DocumentFormSet(queryset=Document.objects.none())
+
+    return render(request, 'oids/document_form.html', {
+        'header_form': header_form,
+        'formset': formset
+    })
