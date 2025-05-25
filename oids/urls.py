b56627e8c46f201245cad578e5795360e6f416ed
Dmytro <dnytro9avr@gmail.com>
2025-05-25 17:54:47 +0300

    old code

--- oids/urls.py
+++ oids/urls.py
@@ -1,53 +1,13 @@
-# D:\myFirstCRM\oids\urls.py
-from django.urls import path, include
+# oids/urls.py
+from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('ajax/load-oids/', views.load_oids, name='ajax_load_oids'),
-    path('ajax/get-oids/', views.get_oids_by_unit, name='get_oids_by_unit'),
-    path('ajax/create/', views.create_oid_ajax, name='create_oid_ajax'),
-    path('ajax/load-documents-for-oids/', views.load_documents_for_oids, name='ajax_load_documents_for_oids'),
-    path('ajax/get-oid-status/', views.get_oid_status, name='ajax_get_oid_status'),
-
-    path('ajax/get-work-request-details/', views.get_work_request_details, name='get_work_request_details'),
-    path('ajax/load-technical-tasks-for-oid/', views.load_technical_tasks_for_oid, name='load_technical_tasks_for_oid'),
-
-
-    path('ajax/load-oids-for-units/', views.load_oids_for_units, name='ajax_load_oids_for_units'),
-    path('ajax/load-oids-for-unit/', views.load_oids_for_unit, name='ajax_load_oids_for_unit'),
-
-    path('ajax/get-requests-by-oid/', views.get_requests_by_oid, name='get_requests_by_oid'),
-    path('ajax/get-requests-by-oids/', views.get_requests_by_oids, name='get_requests_by_oids'),
-
-    # path('api/oids/', views.get_oids_by_unit, name='get_oids_by_unit'),
-    # path('api/requests/', views.get_requests_by_oid, name='get_requests_by_oid'),
+    path('', views.main_dashboard, name='main_dashboard'),
+    path('oid/<int:oid_id>/', views.oid_detail_view, name='oid_detail'),
     
-    path('api/oids/', views.get_oids_by_units, name='get_oids_by_units'),
-    path('api/requests/', views.get_requests_by_oids, name='get_requests_by_oids'),
-
-    path('documents/done/', views.document_done, name='document_done'),
-
-
-    path('work/request/', views.work_request, name='work_request'),
-
-
-
-    path('trip_create/', views.trip_create_view, name='trip-create'),
-
-    path('technical_tasks/create/', views.technical_task_create, name='technical_task_create'),
-    path('technical_tasks_list/', views.technical_task_list, name='technical_task_list'),
-
-    # path('oids/create/', веде до посилання http://127.0.0.1:8000/oids/oids/oid_******
-    path('create/', views.create_oid, name='oid_create'),
-    path('status-change/', views.change_oid_status, name='oid_status_change'),
-
-    path('send_doc/cip', views.send_doc_cip, name='send_doc_cip'),
-    path('send_doc/unit/', views.send_doc_unit, name='send_doc_unit'),
-
-
-    path('<int:oid_id>/', views.oid_details, name='oid_details'),
-    path('unit_overview/', views.unit_overview, name='unit_overview'),
-    path('work_requests/', views.work_request_list, name='work_request_list'),
-    path('trip_list/', views.trip_list, name='trip-list'),
+    # URL-и для форм
+    path('request/add/', views.add_work_request, name='add_work_request'),
+    path('trip/plan/', views.plan_trip, name='plan_trip'),
+    path('document/process/', views.add_document_processing, name='add_document_processing'),
 ]
